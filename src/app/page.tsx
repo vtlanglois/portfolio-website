e3428efc63b3c24dab595be42866da134b6a26bc
Vincent Langlois <vtlanglo@gmail.com>
2025-05-22 18:25:42 -0400

    Initial Wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="mr-auto ml-auto max-w-[80%]">
       <main className="">
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus dicta, corporis in corrupti fuga? Molestias est temporibus ratione, placeat id, suscipit repellendus expedita vero non, quod vitae soluta.
