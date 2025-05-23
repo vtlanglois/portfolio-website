70e41d965b6e8df9523d7f7e977b37b5fca9ce2d
Vincent Langlois <vtlanglo@gmail.com>
2025-05-22 22:21:32 -0400

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,24 @@ export default function Home() {
   return (
     <Wrapper>
       <main>
-        <div className="flex flex-col justify-start items-start max-w-xl">
+        <div className="flex flex-col justify-start items-start max-w-xl gap-3">
           <div className="flex flex-row gap-1">
             <HandWavingIcon size="64" weight="duotone" />
             <HandPeaceIcon size="64" weight="duotone" />
             <HandPointingIcon size="64" weight="duotone" mirrored />
           </div>
+          <Text as="h1" variant="heading1">
+            Hiya! I'm
+            <br />
+            <span className="text-blue-500 text-5xl italic">
+              Vincent Langlois,
+            </span>{" "}
+            <br />
+            software engineer.
+          </Text>
           <Text as="p" variant="paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-            consequatur necessitatibus dicta, corporis in corrupti fuga?
-            Molestias est temporibus ratione, placeat id, suscipit repellendus
-            expedita vero non, quod vitae soluta.
+            I build digtital products, and I love to learn new things. I am
+            currently working at Eli Lilly and Company as a senior software engineer.
           </Text>
         </div>
       </main>
--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -4,7 +4,7 @@ interface WrapperProps extends React.PropsWithChildren, React.ComponentProps<'di
 
 export default function Wrapper({ children, className }: WrapperProps ) {
   const classes = clsx(
-    "mx-auto min-h-screen max-w-screen-xl p-3 lg:px-6",
+    "mx-auto min-h-screen max-w-screen-xl px-3 lg:px-12",
     className
   );
   return (
