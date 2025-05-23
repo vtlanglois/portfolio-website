99cf049d69540ca6a412a6b9586899356e60d670
Vincent Langlois <vtlanglo@gmail.com>
2025-05-22 23:46:41 -0400

    Added Header RD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,10 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="">
-          Header
-        </header>
-        {children}
+        <Header />
+        <div className="mt-16 lg:mt-32">
+          {children}
+        </div>
         <footer className="">
           Footer
         </footer>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 flex items-center justify-between bg-blue-300 rounded-b-2xl px-3 py-4 shadow-md lg:px-12">
+      HEADER
+    </header>
+  );
+}
