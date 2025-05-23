db24c6224fa7cbf0837a7f27bdc1c6532d1cd904
Vincent Langlois <vtlanglo@gmail.com>
2025-05-22 21:30:15 -0400

    Added Phosphor Icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Text from "@/components/Text";
+import {
+  HandPeaceIcon,
+  HandWavingIcon,
+  HandPointingIcon,
+} from "@phosphor-icons/react/ssr";
 
 export default function Home() {
   return (
     <div className="mr-auto ml-auto max-w-[80%]">
       <main className="">
+        <div className="flex flex-row gap-1">
+          <HandWavingIcon size="64" weight="duotone" />
+          <HandPeaceIcon size="64" weight="duotone" />
+          <HandPointingIcon size="64" weight="duotone" mirrored />
+        </div>
         <Text as="p" variant="paragraph">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
           consequatur necessitatibus dicta, corporis in corrupti fuga? Molestias
