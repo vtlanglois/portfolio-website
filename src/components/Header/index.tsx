8861788d299c92dddee4243fbb7a9d9d4745839d
Vincent Langlois <vtlanglo@gmail.com>
2025-05-25 22:57:26 -0400

    Tweaked Header Styling to mirror Footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 mx-auto flex max-w-screen-xl items-center justify-between rounded-b-2xl bg-blue-300 px-3 py-4 shadow-md lg:px-12">
+    <header className="sticky top-0 z-50 flex items-center justify-between rounded-b-2xl border-1 border-b-4 border-solid border-blue-400 bg-blue-300 px-3 py-4 shadow-lg lg:px-12 2xl:mx-auto 2xl:max-w-screen-xl">
       HEADER
     </header>
   );
