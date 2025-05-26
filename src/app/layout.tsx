429a180b41967cb0d23066eef7630a6ef283348c
Vincent Langlois <vtlanglo@gmail.com>
2025-05-25 22:57:07 -0400

    Added Footer RD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
       >
         <Header />
         <div className="mt-4 lg:mt-8">{children}</div>
-        <footer className="">Footer</footer>
+        <Footer />
       </body>
     </html>
   );
