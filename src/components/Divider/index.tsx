1c0238e477d5d143762509c17c594adc76ad6a43
Vincent Langlois <vtlanglo@gmail.com>
2025-06-04 23:08:06 -0400

    refactor: changed divider color

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,5 +1,5 @@
 export default function Divider() {
   return (
-    <div className="border-primary-600 dark:border-secondary h-2 w-full border-b-2" />
+    <div className="border-secondary dark:border-secondary h-2 w-full border-b-2" />
   );
 }
