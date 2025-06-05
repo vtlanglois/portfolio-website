b457681be8cf0292a0f4226d029dc359ccc451d4
Vincent Langlois <vtlanglo@gmail.com>
2025-06-04 22:23:35 -0400

    Updated Footer + Header Styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between rounded-t-2xl border-2 border-solid border-blue-400 bg-blue-300 px-3 py-4 shadow-lg lg:px-12 2xl:mx-auto 2xl:max-w-screen-xl">
+    <footer className="flex items-center justify-between rounded-t-lg bg-primary-400 border-2 border-solid border-primary-600 bg-pripx-3 py-4 shadow-lg lg:px-12 2xl:mx-auto 2xl:max-w-screen-xl">
       Footer
     </footer>
   );
