afa51fdca42c7173b83bbd705d5d5e2e19c1e423
Vincent Langlois <vtlanglo@gmail.com>
2025-06-05 21:41:35 -0400

    refactor: made Footer transparent on light mode

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-primary-400 dark:bg-secondary border-primary-600 flex items-center justify-between rounded-t-lg border-2 border-solid px-3 py-4 shadow-lg lg:px-12 2xl:mx-auto 2xl:max-w-screen-xl dark:border-transparent">
+    <footer className="dark:bg-secondary flex items-center justify-between px-3 py-4 lg:px-12 2xl:mx-auto 2xl:max-w-screen-xl dark:border-transparent">
       Footer
     </footer>
   );
