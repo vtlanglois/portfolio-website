40cb3de5d747dfa5d7961485842e7e62b56cf73b
Vincent Langlois <vtlanglo@gmail.com>
2025-06-14 18:07:26 -0400

    feat: add Footer structure

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,22 @@
 import Container from "@/components/Container";
+import Text from "@/components/Text";
+import { GitHubIconLink, LinkedInIconLink } from "@/components/SocialIconLink";
+import Divider from "../Divider";
 
 export default function Footer() {
   return (
-    <footer className="dark:bg-red px-3 py-4 lg:px-16 2xl:mx-auto 2xl:max-w-screen-xl dark:border-transparent">
-      <Container></Container>
+    <footer className="dark:bg-red bg-primary-400 px-3 py-4 lg:px-16 2xl:mx-auto 2xl:max-w-screen-xl dark:border-transparent">
+      <Container className="flex flex-col gap-3">
+        <div className="flex flex-row items-center justify-between">
+          <Text as="p" variant="paragraph">
+            2025 - Vincent Langlois
+          </Text>
+          <div className="flex flex-row gap-3">
+            <GitHubIconLink className="dark:hover:!bg-black" />
+            <LinkedInIconLink className="dark:hover:!bg-black" />
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 }
--- src/components/SocialIconLink/index.tsx
+++ src/components/SocialIconLink/index.tsx
@@ -3,26 +3,28 @@
 import NavLink from "@/components/NavLink";
 import { GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
 
-export function GitHubIconLink() {
+export function GitHubIconLink({ className }: React.HTMLAttributes<"a">) {
   return (
     <NavLink
       variant="icon"
       appearance="primary"
       href="https://github.com/vtlanglois"
       aria-label="Github"
+      className={className}
     >
       <GithubLogoIcon size="32" weight="bold" />
     </NavLink>
   );
 }
 
-export function LinkedInIconLink() {
+export function LinkedInIconLink({ className }: React.HTMLAttributes<"a">) {
   return (
     <NavLink
       variant="icon"
       appearance="primary"
       href="https://www.linkedin.com/in/vtlanglois/"
       aria-label="LinkedIn"
+      className={className}
     >
       <LinkedinLogoIcon size="32" weight="bold" />
     </NavLink>
