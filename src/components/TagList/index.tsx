40ca7c98bcd465dd2ed6b857a9ac8d097a44a23e
Vincent Langlois <vtlanglo@gmail.com>
2025-06-21 21:34:27 -0400

    feat: optimized taglist

--- src/components/TagList/index.tsx
+++ src/components/TagList/index.tsx
@@ -2,6 +2,7 @@ import Tag from "@/components/Tag";
 import type { TagVariant, TagItem } from "@/types/tagTypes";
 import { sortTags } from "@/utils/tagUtils";
 import clsx from "clsx";
+import { useMemo } from "react";
 
 interface TagListProps extends React.HTMLAttributes<HTMLUListElement> {
   /** Tags to render in list format */
@@ -18,15 +19,16 @@ export default function TagList({
   ariaLabel,
   className,
 }: TagListProps) {
-  const classes = clsx("flex flex-wrap gap-x-2 gap-y-3", className);
+  const classes = useMemo(() => {
+    return clsx("flex flex-wrap gap-x-2 gap-y-3", className);
+  }, [className]);
 
-  const sortedTags = sortTags(tags);
-  console.log("Sorted Tags:", sortedTags);
+  const sortedTags = useMemo(() => sortTags(tags), [tags]);
 
   return (
     <ul className={classes} aria-label={ariaLabel}>
-      {sortedTags.map((tag, index) => (
-        <li key={index}>
+      {sortedTags.map((tag) => (
+        <li key={`${tag.text}-${tag.variant || defaultVariant}`}>
           <Tag variant={tag.variant || defaultVariant} icon={tag.icon}>
             {tag.text}
           </Tag>
