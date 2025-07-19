f4e283dd07bff0f4dfd85dd5843a1b65cc8c4085
Vincent Langlois <vtlanglo@gmail.com>
2025-07-19 19:22:40 -0400

    feat: added details page RD

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import projects from "@/data/projects";
 import { DetailedProjectItem } from "@/types/projectTypes";
+import Article from "@/components/Article";
+import Text from "@/components/Text";
+import TagList from "@/components/TagList";
+import Stack from "@/components/Stack";
+import { useCallback } from "react";
 
 export default async function Page({
   params,
@@ -7,7 +12,29 @@ export default async function Page({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  return <div>My Project: {slug}</div>;
+  const project = projects.find(
+    (p): p is DetailedProjectItem =>
+      "slug" in p && typeof p.slug === "string" && p.slug === slug,
+  );
+
+  return (
+    <Article>
+      <div className="col-span-3">
+        <Text as="h1" variant="heading1">
+          {project?.name}
+        </Text>
+        <div className="mt-4">{project?.body}</div>
+      </div>
+      <aside className="col-span-2">
+        <Stack>
+          <Text as="h2" variant="heading3">
+            Technologies & Topics
+          </Text>
+          <TagList tags={project?.tags || []} />
+        </Stack>
+      </aside>
+    </Article>
+  );
 }
 
 export async function generateStaticParams() {
